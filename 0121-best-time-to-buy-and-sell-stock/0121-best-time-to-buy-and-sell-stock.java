0eae55240b2ea32415699c60d90990b3afad8e64
Somrik Sur <126485516+Somriksur@users.noreply.github.com>
2024-04-01 16:05:21 +0530

    Time: 2 ms (78.28%), Space: 61.7 MB (50.75%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i=0,j=1;
+        int max=0;
+        while(j<prices.length){
+            if(prices[j]<prices[i]){
+                i=j;
+                j++;
+            }
+            else{
+            max=Math.max(max,prices[j]-prices[i]);
+            j++;
+            }
+        }
+        return max;
+    }
+}
