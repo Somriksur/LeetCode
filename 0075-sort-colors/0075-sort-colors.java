d92b728e30362a4f21c1e682898197569b36792f
Somrik Sur <126485516+Somriksur@users.noreply.github.com>
2025-09-02 04:02:55 +0530

    Time: 15 ms (3.27%), Space: 42.5 MB (5.11%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,16 +1,46 @@
-class Solution {
-    public void sortColors(int[] nums) {
-        int zeros = 0, ones  = 0, n = nums.length;
+import java.util.Scanner;
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0) zeros++;
-            if(nums[i] == 1) ones++;
+public class Solution {
+
+    // Static method to print the array
+    public static void printArray(int[] arr) {
+        for(int num : arr) {
+            System.out.print(num + " ");
         }
+        System.out.println();
+    }
+
+    // Bubble sort logic to sort the array
+    public void sortColors(int[] nums) {
+        for(int i = 0; i < nums.length - 1; i++) {
+            for(int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    // Swap
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+
+        printArray(nums);
+    }
+
+    // Main method to take user input and call sorting
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the array elements:");
 
         for(int i = 0; i < n; i++) {
-            if(i < zeros) nums[i] = 0;
-            if(zeros <= i && i < zeros + ones) nums[i] = 1;
-            if(i >= zeros + ones) nums[i] = 2;
+            nums[i] = sc.nextInt();
         }
+
+        Solution sol = new Solution();
+        sol.sortColors(nums);
     }
 }
